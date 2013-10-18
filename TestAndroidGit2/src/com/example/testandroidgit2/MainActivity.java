bdda939b6bad2a5185d98072c7d80ce561774011
budiraharja <ib.budi.raharja@gmail.com>
2013-10-18 11:11:52 +0700

    test edit

--- TestAndroidGit2/src/com/example/testandroidgit2/MainActivity.java
+++ TestAndroidGit2/src/com/example/testandroidgit2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.testandroidgit2;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +18,12 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		return super.onOptionsItemSelected(item);
 	}
 
 }
